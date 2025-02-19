b3d294edbee93e1e497d97c3df9df515f5e75444
Rodrigo Rabello <rodrigo.azevedo.rabello@gmail.com>
2025-02-18 21:15:44 -0300

    Start

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+      <main>
+        <h1>Origamid Home</h1>
+      </main>
+
+  );
+}
